9ce10c0d4e1ece44cc0ffe19487f06cc021795a2
Deej Potter <deejpotter@gmail.com>
2021-01-04 21:03:08 +1100

    Added files for the new phaser game.

---
+++ work/new-phaser.php
@@ -0,0 +1,30 @@
+<?php 
+$fileLevel = "../";
+$title = "New phaser game | Deej Potter Designs";
+include "../templates/header.php" 
+?>
+
+<main id="main">	
+
+    <!-- Hero section -->
+	<?php
+		$bgColour = "primary";
+		$bgImage = "../images/pricing-image-50-900x600.png";
+		$textColour = "light";
+		$sectionH1 = "New phaser game";
+		$sectionP = "This is a new phaser game.";
+		$otherClasses = "inset-background-cover";
+		include "../templates/hero.php"
+    ?>	
+
+    <!-- Content section -->
+    <section class="container">
+        <div id="game-area" class=""></div>
+
+        <script src="<?php echo $fileLevel ?>js/phaser.js"></script>
+        <script src="<?php echo $fileLevel ?>js/new-phaser.js"></script>
+    </section>
+	
+</main>
+
+<?php include "../templates/footer.php" ?>
